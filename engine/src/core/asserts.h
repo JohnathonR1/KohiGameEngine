4e738b2f40d09ba045eb83e3be3fc30b12ac0b03
JohnathonR1 <Johnathon.and.emerson@gmail.com>
2024-03-24 07:27:48 -0500

    episode 3

---
+++ engine/src/core/asserts.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "defines.h"
+
+// Disabled assertions by commenting line below
+#define KASSERTIONS_ENABLED
+
+#ifdef KASSERTIONS_ENABLED
+#if _MSC_VER
+#include <intrin.h>
+#define debugBreak() __debugbreak()
+#else
+#define debugBreak() __builtin_trap()
+#endif
+
+KAPI void report_assertion_faliure(const char* espression, const char* message, const char* file, i32 line);
+
+#define KASSERT(expr)                                                \
+    {                                                                \
+        if (expr) {                                                  \
+        } else {                                                     \
+            report_assertion_faliure(#expr, "", __FILE__, __LINE__); \
+            debugBreak();                                            \
+        }                                                            \
+    }                         
+    
+#define KASSERT_MSG(expr, message)                                        \
+    {                                                                     \
+        if (expr) {                                                       \
+        } else {                                                          \
+            report_assertion_failure(#expr, message, __FILE__, __LINE__); \
+            debugBreak();                                                 \
+        }                                                                 \
+    }
+
+#ifdef _DEBUG
+#define KASSERT_DEBUG(expr)                                          \
+    {                                                                \
+        if (expr) {                                                  \
+        } else {                                                     \
+            report_assertion_failure(#expr, "", __FILE__, __LINE__); \
+            debugBreak();                                            \
+        }                                                            \
+    }
+#else
+#define KASSERT_DEBUG(expr) // Does nothing
+#endif
+
+#else
+#define KASSERT(expr)               // Does nothing
+#define KASSERT_MSG(expr, message)  // Does nothing
+#define KASSERT_DEBUG(expr)         // Does nothing
+#endif
--- engine/src/test.c
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-
-#include <stdio.h>
-
-// Prints a number into the console
-void print_int(i32 i) {
-    printf("The number is: %i\n", i);
-}
