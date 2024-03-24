ce9f8f113025af311bf35cd12d828e0bfa8438e7
JohnathonR1 <Johnathon.and.emerson@gmail.com>
2024-03-24 11:37:58 -0500

    episode 7

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -1,6 +1,7 @@
 #include "game.h"
 
 #include <entry.h>
+#include <core/kmemory.h>
 
 // TODO: Remove this
 #include <platform/platform.h>
@@ -19,7 +20,7 @@ b8 create_game(game* out_game) {
     out_game->on_resize = game_on_resize;
 
     // Create the game state.
-    out_game->state = platform_allocate(sizeof(game_state), FALSE);
+    out_game->state = kallocate(sizeof(game_state), MEMORY_TAG_GAME);
 
     return TRUE;
 }
